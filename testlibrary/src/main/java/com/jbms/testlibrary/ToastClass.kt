a4bbd7cab7d6d7dbc0ac68897444faa60408bbe9
jubongoh <jubong.oh@gmail.com>
2022-08-12 11:40:58 +0900

    library source 추가

---
+++ testlibrary/src/main/java/com/jbms/testlibrary/ToastClass.kt
@@ -0,0 +1,10 @@
+package com.jbms.testlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToastClass {
+    companion object {
+        fun showToast(context: Context, message:String) = Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+}
