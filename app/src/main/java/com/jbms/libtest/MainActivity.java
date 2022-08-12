91ba0f40dc9b2803780ce36822ae8bdcbcd120b8
jubongoh <jubong.oh@gmail.com>
2022-08-12 17:26:23 +0900

    module activity 추가

--- app/src/main/java/com/jbms/libtest/MainActivity.java
+++ app/src/main/java/com/jbms/libtest/MainActivity.java
@@ -2,8 +2,12 @@ package com.jbms.libtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.jbms.testlibrary.LibTestActivity;
 import com.jbms.testlibrary.ToastClass;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ToastClass.showToast(this, "aaaaaaaa");
+
+        Button btn = findViewById(R.id.btn_text);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LibTestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ testlibrary/src/main/java/com/jbms/testlibrary/LibTestActivity.kt
@@ -0,0 +1,11 @@
+package com.jbms.testlibrary
+
+import android.app.Activity
+import android.os.Bundle
+
+class LibTestActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_libtest)
+    }
+}
