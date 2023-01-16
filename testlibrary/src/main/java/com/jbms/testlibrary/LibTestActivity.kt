4569a1f3456d656a0767c8367683e622f0006286
jubongO <111949991+jubongO@users.noreply.github.com>
2023-01-16 14:06:59 +0900

    lib build config

--- testlibrary/src/main/java/com/jbms/testlibrary/LibTestActivity.kt
+++ testlibrary/src/main/java/com/jbms/testlibrary/LibTestActivity.kt
@@ -2,10 +2,13 @@ package com.jbms.testlibrary
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class LibTestActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_libtest)
+        var tv = findViewById<TextView>(R.id.tv_version)
+        tv.text = "META-VER : ${BuildConfig.TESTLIB_SDK_VERSION}"
     }
 }
