20c68cceb5896fb2cf3ea57e1ee10a0fe6d97195
blackzombie <blackzom@gmail.com>
2023-03-05 09:44:44 +0900

    dokka 실행
    Gradle -> testlibrary -> dokkaHtml 실행

--- testlibrary/src/main/java/com/jbms/testlibrary/LibTestActivity.kt
+++ testlibrary/src/main/java/com/jbms/testlibrary/LibTestActivity.kt
@@ -3,6 +3,9 @@ package com.jbms.testlibrary
 import android.app.Activity
 import android.os.Bundle
 
+/**
+ * @suppress
+ */
 class LibTestActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
